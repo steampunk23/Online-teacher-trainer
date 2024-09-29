a2a03b68abcd4ac4ac105b10506e37b38cea7ee2
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2024-09-29 18:19:17 +0530

    delete kehan's files from my branch and change design in payment page

--- trainer.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('edit-btn').addEventListener('click', function() {
-    const inputs = document.querySelectorAll('.account-info input');
-    inputs.forEach(input => {
-        input.disabled = !input.disabled;
-        if (!input.disabled) {
-            input.style.animation = 'fieldSlideIn 1.5s ease-in-out forwards';
-        }
-    });
-});
