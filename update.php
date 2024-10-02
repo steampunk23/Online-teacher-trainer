07d98cdab67c83ceed8e646af598f363061349cf
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2024-10-02 17:24:23 +0530

    design update message box

--- update.php
+++ update.php
@@ -1,3 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Message</title>
+
+    <style>
+
+        .update_container{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .update_title{
+            font-size: 20px;
+            padding-bottom: 10px;
+        }
+
+        label{
+            display: block;
+        }
+
+        .update_message_box {
+            width: 600px;
+            height: auto;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+            margin-bottom: 10px;
+        }
+
+        .button{
+            display: block;
+            padding: 5px 10px;
+            margin-right: 5px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        .update_message_box:focus {
+            border-color: lightgreen;
+            outline: none;
+        }
+    </style>
+</head>
+<body>
+
+<div class="update_container">
+
 <?php
 
 //data base connction
@@ -20,9 +78,9 @@ if(isset($_GET['msg_id'])){
     // form
     echo "<form action='update_insert.php' method='post'>";
     echo "<input type='hidden' name='msg_id' value='$msg_id'>";             
-    echo "<label>Message :</label>";
-    echo "<textarea cols='30' rows='8' placeholder='Your message' name='message'>" . $message . "</textarea>";   
-    echo "<input type='submit' value='submit' class='contact_us_button'>";      
+    echo "<label class='update_title' >Update Your Message :</label>";
+    echo "<textarea class='update_message_box' cols='30' rows='8' placeholder='Your message' name='message'>" . $message . "</textarea>";   
+    echo "<input class='button' type='submit' value='submit' class='contact_us_button'>";      
     echo "</form>";}
     
 
@@ -35,11 +93,8 @@ else {
     echo "ID parameter is misiing";
 }
 
-
-
-
-
-
-
-
 ?>
+
+</div>
+</body>
+</html>
