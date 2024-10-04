46f6cecc73aa35ffe528b77b1a4581e577813179
steampunk23 <pathumbandara1234@gmail.com>
2024-10-04 23:25:30 +0530

    login pages connect to the database

---
+++ teacherlogindatainsert.php
@@ -0,0 +1,25 @@
+<?php
+
+    require 'config.php';
+
+    $email = $_POST['logemail'];
+    $password = $_POST['logpassword'];
+
+    $stmt = $con -> prepare("Select * from teacher where email = ?");
+    $stmt  -> bind_param("s", $email);
+    $stmt -> execute();
+    $stmt_result = $stmt -> get_result();
+
+
+    if($stmt_result -> num_rows > 0) {
+        $data = $stmt_result -> fetch_assoc();
+        if($data['password'] === $password) {
+            echo "<h2>Login successfully</h2>";
+        } else {
+            echo "<h2>Invalid Email or password</h2>";
+        }
+    } else {
+        echo "<h2>Invalid Email or password</h2>";
+    }
+
+?>
