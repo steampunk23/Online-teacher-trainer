85957e35917b08ebe7b243411d085648d9373ab4
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2024-10-07 02:18:26 +0530

    delete unwanted files

--- Regselector.js
+++
@@ -1,7 +0,0 @@
-function pageselecotr(regType) {
-    if(regType === 'teacher') {
-        location.href = "course.html";
-    } else if(regType === 'trainer') {
-        location.href = "Art.html";
-    }
-}
